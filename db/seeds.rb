5e8e88bcf1ed80cae15183e3e248def915055c7d
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-09 13:56:03 -0400

    Wrote a seed file

---
+++ db/seeds.rb
@@ -0,0 +1,182 @@
+Song.find_or_create_by(
+  name: "Snow",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+
+Song.find_or_create_by(
+  name: "Californication",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Can't Stop",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Otherside",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Scar Tissue",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Under the Bridge",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Dani California",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Dark Necesities",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "The Zephyr Song",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "By the Way",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+
+Song.find_or_create_by(
+  name: "Don't Forget Me",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Suck My Kiss",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "The Longest Wave",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Carlie",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Hey",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Good Time Boys",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Desecration Smile",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Venice Queen",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Goodbye Angels",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Go Robot",
+  artist: "Red Hot Chile Peppers",
+  duration: 5
+  )
+  
+
+Song.find_or_create_by(
+  name: "Soul Meets Body",
+  artist: "Death Cab For Cutie",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Monday Morning",
+  artist: "Death Cab For Cutie",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Stupid Love",
+  artist: "Lady Gaga",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Break My Heart",
+  artist: "Dua Lipa",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "No Time To Die",
+  artist: "Billie Eilish",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Intentions",
+  artist: "Justin Bieber",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Godzilla",
+  artist: "Eminem",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Breathe Deeper",
+  artist: "Tame Impala",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Midnight Sky",
+  artist: "Miley Cyrus",
+  duration: 5
+  )
+  
+Song.find_or_create_by(
+  name: "Go Crazy",
+  artist: "Chris Brown",
+  duration: 5
+  )
+  
